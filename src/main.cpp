483d9f4ddbd5b2247719068b74a52a55d18d7a36
Filebin <alex.mousse9@gmail.com>
2022-03-13 10:48:06 +0300

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     auto utf8 = std::locale(std::locale(), new std::codecvt_utf8<wchar_t>);
     file.imbue(utf8);
     setlocale(LC_ALL, "");
-    Lexer<wchar_t> lexer(file);
+    Lexer<> lexer(file);
     Token token;
     while((token = lexer.GetNextToken()).ty != Token::End)
         wprintf(L"Read token %ls\n", token.ToString().c_str());
